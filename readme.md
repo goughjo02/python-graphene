5e31df3b897101e10388f7ca20250ad581a20e26
goughjo02 <goughjo02@gmail.com>
2021-04-10 20:11:59 +0100

    Initial commit

---
+++ readme.md
@@ -0,0 +1,18 @@
+# Python Graphene
+
+## Setup
+
+```
+pip install -r requirements.txt
+```
+
+## Contributing
+
+### pyenv + pyenv-virtualenv
+
+#### To install new packages
+
+```
+pip install <package-name>
+pip freeze > requirements.txt
+```
---
+++ schema.py
@@ -0,0 +1,29 @@
+import graphene
+import json
+
+class Query(graphene.ObjectType):
+    hello = graphene.String()
+    is_admin = graphene.Boolean()
+
+    def resolve_hello(self, info):
+        return "world"
+
+    def resolve_is_admin(self, info):
+        return True
+
+schema = graphene.Schema(query=Query)
+
+result = schema.execute(
+    '''
+    {
+        hello
+        isAdmin
+    }
+    '''
+)
+
+print(result)
+
+dictResult = dict(result.data.items())
+
+print(json.dumps(dictResult, indent=2))
